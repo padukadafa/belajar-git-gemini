6834e204ae8296358d06d194267a628c3596483f
padukadafa <padukadafa@gmail.com>
2026-05-01 13:18:15 +0700

    Menambahkan  fitur kamera kanan dan kiri

--- belajar-git-gemini/ContentView.swift
+++ belajar-git-gemini/ContentView.swift
@@ -3,17 +3,18 @@
 //  belajar-git-gemini
 //
 //  Created by Daniel Ferryal Zuhri on 01/05/26.
-//
+// asd
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("Kamera Kanan")
+                Spacer()
+                Text("Kamera Kiri")
+            }
         }
         .padding()
     }
--- belajar-git-gemini/belajar_git_geminiApp.swift
+++ belajar-git-gemini/belajar_git_geminiApp.swift
@@ -2,7 +2,7 @@
 //  belajar_git_geminiApp.swift
 //  belajar-git-gemini
 //
-//  Created by Daniel Ferryal Zuhri on 01/05/26.
+//  Created by Daniel Ferryal Zuhri (Ryujin) on 01/05/26.
 //
 
 import SwiftUI
